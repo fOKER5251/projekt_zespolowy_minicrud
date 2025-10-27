dd7294c64943b69efff50db3929dac8626c685f1
fOKER5251 <stavs.w.vlad.w@gmail.com>
2025-10-27 01:18:00 +0100

    Dodano komentarze wyjaśniające w kodzie (przygotowanie do obrony)

--- app/models.py
+++ app/models.py
@@ -1,11 +1,16 @@
+# MODEL DANYCH — STUDENT
+# Ten plik definiuje strukturę danych (tabelę w bazie).
+# Dzięki SQLAlchemy tworzymy model obiektowy (ORM),
+# który automatycznie zamienia dane Python  SQLite.
+
 from sqlalchemy import Column, Integer, String
 from .database import Base
 
 class Student(Base):
     __tablename__ = "students"
-    id = Column(Integer, primary_key=True)
-    first_name = Column(String(80), nullable=False)
-    last_name = Column(String(80), nullable=False)
-    group_code = Column(String(40), nullable=False)
+    id = Column(Integer, primary_key=True)  # unikalne ID (klucz główny)
+    first_name = Column(String(80), nullable=False)  # imię studenta (nie może być puste)
+    last_name = Column(String(80), nullable=False)  # nazwisko studenta
+    group_code = Column(String(40), nullable=False)  # symbol grupy studenckiej
 
-# TODO: dodać walidację danych studenta 
+# TODO: można dodać walidację danych (np. długość imienia lub format group_code)
